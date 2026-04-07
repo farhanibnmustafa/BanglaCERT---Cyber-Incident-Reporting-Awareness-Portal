6488bce8789c6d13e841f10ee5486d9d34c948ea
farhanibnmustafa <farhanibnmustafa@gmail.com>
2026-04-07 15:39:12 +0600

    Empty main branch

--- BanglaCERT/auditlog/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuditlogConfig(AppConfig):
-    name = 'auditlog'
-3
--- BanglaCERT/awareness/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AwarenessConfig(AppConfig):
-    name = 'awareness'
