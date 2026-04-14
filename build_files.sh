ea6f8f0d72e37732b37cc66f3364f77da55f0ce2
farhanibnmustafa <farhanibnmustafa@gmail.com>
2026-04-15 01:53:37 +0600

    Add migrations to Vercel build script

--- build_files.sh
+++ build_files.sh
@@ -12,5 +12,6 @@ cd ..
 # Build Django backend
 echo "Building Django backend..."
 python3 -m pip install -r requirements.txt --break-system-packages
+python3 BanglaCERT/manage.py migrate --noinput
 python3 BanglaCERT/manage.py collectstatic --noinput --clear
 echo "BUILD END"
